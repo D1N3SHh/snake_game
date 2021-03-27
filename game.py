ffd08b498f8679696686e67ff3a363a64619bd28
D1N3SH_ <tomekjelito@gmail.com>
2021-03-27 18:00:34 +0100

    Add snake movement and apple spawning

--- game.py
+++ game.py
@@ -2,27 +2,46 @@
 
 import pygame
 import sys
+import random
 
 pygame.init()
+
 def run():
-    box = pygame.Rect(600,250,7,7)
-    screen = pygame.display.set_mode((1200,500))
+    y = 1
+    x = 0
+    height = random.randint(0,1920)
+    width = random.randint(0,1080)
+    food = pygame.Rect(height,width,25,25)
+    snake = pygame.Rect(800,250,50,50)
+    screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit(0)
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            box.y -= 1
+            y = -1 # -1
+            x = 0
         if keys[pygame.K_s]:
-            box.y += 1
+            y = 1 # 1
+            x = 0
         if keys[pygame.K_a]:
-            box.x -= 1
+            x = -1 # -1
+            y = 0
         if keys[pygame.K_d]:
-            box.x += 1
+            x = 1 # 1
+            y = 0
+
+        snake.y += y
+        snake.x += x
+
+        if snake.y == height:
+            print("Done")
 
         screen.fill((0,0,0))
-        pygame.draw.rect(screen,(0,255,0),box)
+        pygame.draw.rect(screen,(0,255,0),snake)
+        pygame.draw.rect(screen,(255,0,0),food)
         pygame.display.flip()
