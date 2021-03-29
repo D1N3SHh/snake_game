c6aa4f9d296d4b46c94d029d82e7e7366c38ff00
Piotr Jakubczyk <piotr@localhost.localdomain>
2021-03-29 22:58:11 +0200

    Apple is reborning and snake is trapped

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 import pygame
 import sys
 import random
+import time
 
 pygame.init()
 
@@ -13,12 +14,18 @@ def run():
     delta = 0.0
     max_tps = 10
 
-    y = 1
+    y = 10
     x = 0
-    height = random.randint(0,1920)
-    width = random.randint(0,1080)
-    food = pygame.Rect(height,width,25,25)
-    snake = pygame.Rect(800,250,50,50)
+    while True:
+        height = random.randint(0,1920)
+        width = random.randint(0,1080)
+        if height%10 == 0 and width%10 == 0:
+            food = pygame.Rect(height,width,10,10)
+            break
+        else:
+            continue
+    food = pygame.Rect(height,width,10,10)
+    snake = pygame.Rect(800,250,10,10)
     screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
     while True:
         for event in pygame.event.get():
@@ -34,23 +41,38 @@ def run():
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
-                y = -1 # -1
+                y = -10
                 x = 0
             if keys[pygame.K_s]:
-                y = 1 # 1
+                y = 10
                 x = 0
             if keys[pygame.K_a]:
-                x = -1 # -1
+                x = -10
                 y = 0
             if keys[pygame.K_d]:
-                x = 1 # 1
+                x = 10
                 y = 0
 
         snake.y += y
         snake.x += x
-
-        if snake.y == height:
-            print("Done")
+        time.sleep(0.05)
+        if snake.y == width and snake.x == height:
+            while True:
+                height = random.randint(0,1920)
+                width = random.randint(0,1080)
+                if height%10 == 0 and width%10 == 0:
+                    food = pygame.Rect(height,width,10,10)
+                    break
+                else:
+                    continue
+        if snake.x > 1921:
+            snake.x = 0
+        if snake.x < 0:
+            snake.x = 1920
+        if snake.y < 0:
+            snake.y = 1080
+        if snake.y > 1080:
+            snake.y = 0
 
         screen.fill((0,0,0))
         pygame.draw.rect(screen,(0,255,0),snake)
