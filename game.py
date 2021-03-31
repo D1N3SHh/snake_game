615142db9a0e3e6da1238ded24c5489a14574088
Piotr Jakubczyk <piotr@localhost.localdomain>
2021-03-31 21:19:21 +0200

    Added comments

--- game.py
+++ game.py
@@ -13,14 +13,14 @@ def run():
     clock = pygame.time.Clock()
     delta = 0.0
     max_tps = 10
-
+    #Constant values
     y = 10
     x = 0
     while True:
         height = random.randint(0,1920)
         width = random.randint(0,1080)
+        #Checking height and width to spawn apple at right place
         if height%10 == 0 and width%10 == 0:
-            food = pygame.Rect(height,width,10,10)
             break
         else:
             continue
@@ -28,6 +28,7 @@ def run():
     snake = pygame.Rect(800,250,10,10)
     screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
     while True:
+        #Checking output keys to exit program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
@@ -39,6 +40,7 @@ def run():
         while delta > 1 / max_tps:
             delta -= 1 / max_tps
 
+            #Snake turning
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
                 y = -10
@@ -56,6 +58,7 @@ def run():
         snake.y += y
         snake.x += x
         time.sleep(0.05)
+        #Creating apple after eating
         if snake.y == width and snake.x == height:
             while True:
                 height = random.randint(0,1920)
@@ -65,6 +68,7 @@ def run():
                     break
                 else:
                     continue
+        #This should be deleted
         if snake.x > 1921:
             snake.x = 0
         if snake.x < 0:
