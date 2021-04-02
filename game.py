2b06b5994eb1d3a2366a9a5f840da1e47c501a76
D1N3SH_ <tomekjelito@gmail.com>
2021-04-02 22:50:05 +0200

    Bigger objects

--- game.py
+++ game.py
@@ -30,26 +30,26 @@ def run():
     #Tickrate values
     clock = pygame.time.Clock()
     delta = 0.0
-    max_tps = 100
+    max_tps = 60
 
     #Constant values
-    y = 10
-    x = 0
+    y = 0
+    x = 50
     score = 1
     body = []
-    direction = "down"
+    direction = "right"
     global screen
 
     while True:
         height = random.randint(0,1920)
         width = random.randint(0,1080)
         #Checking height and width to spawn apple at right place
-        if height%10 == 0 and width%10 == 0:
+        if height%50 == 0 and width%50 == 0:
             break
         else:
             continue
-    food = pygame.Rect(height,width,10,10)
-    snake = pygame.Rect(800,250,10,10)
+    food = pygame.Rect(height,width,50,50)
+    snake = pygame.Rect(0,0,50,50)
     screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
     while True:
         #Checking output keys to exit program
@@ -68,49 +68,50 @@ def run():
             keys = pygame.key.get_pressed()
             if direction == "down" or direction == "up":
                 if keys[pygame.K_a]:
-                    x = -10
+                    x = -50
                     y = 0
                     direction = "left"
                 if keys[pygame.K_d]:
-                    x = 10
+                    x = 50
                     y = 0
                     direction = "right"
             elif direction == "left" or direction == "right":
                 if keys[pygame.K_w]:
-                    y = -10
+                    y = -50
                     x = 0
                     direction = "up"
                 if keys[pygame.K_s]:
-                    y = 10
+                    y = 50
                     x = 0
                     direction = "down"
-            
+        time.sleep(0.2)
         snake.y += y
         snake.x += x
-        time.sleep(0.05)
+        
         #Creating apple after eating
         if snake.y == width and snake.x == height:
             score +=1
             while True:
                 height = random.randint(0,1920)
                 width = random.randint(0,1080)
-                if height%10 == 0 and width%10 == 0:
-                    food = pygame.Rect(height,width,10,10)
+                if height%50 == 0 and width%50 == 0:
+                    food = pygame.Rect(height,width,50,50)
                     break
                 else:
                     continue
-        #This should be deleted #So why is it here?
-        if snake.x > 1921:
-            snake.x = 0
+                
+        #Band colison detection
+        if snake.x > 1920:
+            sys.exit(0)
         if snake.x < 0:
-            snake.x = 1920
+            sys.exit(0)
         if snake.y < 0:
-            snake.y = 1080
+            sys.exit(0)
         if snake.y > 1080:
-            snake.y = 0
+            sys.exit(0)
 
         screen.fill((0,0,0))
-        pygame.draw.rect(screen,(0,255,0),snake)
+        pygame.draw.rect(screen,(122,0,122),snake)
         snake_body(snake,body,score)
         pygame.draw.rect(screen,(255,0,0),food)
         pygame.display.flip()
